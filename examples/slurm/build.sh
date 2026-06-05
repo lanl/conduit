713a11a0c1e2afd0e471801edab291cef58b53d9
Kevin Pelzel <kpelzel@lanl.gov>
2026-06-05 09:04:52 -0500

    move images to an images dir

--- examples/slurm/build.sh
+++ examples/slurm/build.sh
@@ -1,54 +1,55 @@
 #!/bin/sh
 set -e
 
-DEFAULT_SLURM_VER=24.05.8
-SLURM_VER="${SLURM_VER:-$DEFAULT_SLURM_VER}"
-MUNGE_VER=0.5.16
+if [ $(id -u) -ne 0 ]; then
+	echo "Please run as root." >&2
+	exit 1
+fi
 
-echo munge-build:
-$DOCKER build \
-	-t munge-build:$MUNGE_VER \
-	-f Dockerfile.munge-build \
-	--build-arg MUNGE_VER=$MUNGE_VER \
-	.
-echo mungekey:
-$DOCKER build \
-	-t mungekey:$MUNGE_VER \
-	-t mungekey:latest \
-	-f Dockerfile.mungekey \
-	--build-arg MUNGE_VER=$MUNGE_VER \
-	.
-echo munged:
-$DOCKER build \
-	-t munged:$MUNGE_VER \
-	-f Dockerfile.munged \
-	--build-arg MUNGE_VER=$MUNGE_VER \
-	.
-echo slurm-build:
-$DOCKER build \
-	-t slurm-build:$SLURM_VER \
-	-f Dockerfile.slurm-build \
-	--build-arg MUNGE_VER=$MUNGE_VER \
-	--build-arg SLURM_VER=$SLURM_VER \
-	.
-echo slurmctld:
-$DOCKER build \
-	-t slurmctld:$SLURM_VER \
-	-f Dockerfile.slurmctld \
-	--build-arg MUNGE_VER=$MUNGE_VER \
-	--build-arg SLURM_VER=$SLURM_VER \
-	.
-echo slurmd:
-$DOCKER build \
-	-t slurmd:$SLURM_VER \
-	-f Dockerfile.slurmd \
-	--build-arg MUNGE_VER=$MUNGE_VER \
-	--build-arg SLURM_VER=$SLURM_VER \
-	.
-echo slurmrestd:
-$DOCKER build \
-	-t slurmrestd:$SLURM_VER \
-	-f Dockerfile.slurmrestd \
-	--build-arg MUNGE_VER=$MUNGE_VER \
-	--build-arg SLURM_VER=$SLURM_VER \
-	.
+SLURM_DOCKER_PATH="/etc/slurm_docker"
+
+# get path of script
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
+echo SCRIPT_DIR:$SCRIPT_DIR
+
+# Get "global" variables
+. "${SCRIPT_DIR}"/vars.sh
+
+# create slurm_docker area
+mkdir -p $SLURM_DOCKER_PATH
+mkdir -p $SLURM_DOCKER_PATH/munge
+
+# generate ssh keys for testuser
+mkdir -p $SLURM_DOCKER_PATH/ssh/testuser
+rm -f $SLURM_DOCKER_PATH/ssh/testuser/id_rsa
+rm -f $SLURM_DOCKER_PATH/ssh/testuser/id_rsa.pub
+ssh-keygen -f $SLURM_DOCKER_PATH/ssh/testuser/id_rsa -C testuser@slurm_docker_example -q -N ""
+cat $SLURM_DOCKER_PATH/ssh/testuser/id_rsa.pub > $SLURM_DOCKER_PATH/ssh/testuser/authorized_keys
+chmod 600 $SLURM_DOCKER_PATH/ssh/testuser/authorized_keys
+chown -R 1433:1433 $SLURM_DOCKER_PATH/ssh/testuser
+
+# Check for Docker
+$DOCKER --version || echo "Docker not found: '$DOCKER'" >&2
+
+# build slurm-docker images
+cd $SCRIPT_DIR/images/
+$SCRIPT_DIR/images/build.sh
+cd $SCRIPT_DIR
+
+# generate munge key
+chmod 777 $SLURM_DOCKER_PATH/munge
+$DOCKER run --rm -v $SLURM_DOCKER_PATH/munge:/etc/munge mungekey:latest --force
+chmod 755 $SLURM_DOCKER_PATH/munge
+
+# build and run slurm_docker example images
+cd $SCRIPT_DIR
+$DOCKER_COMPOSE build
+
+# Correct permissions on munge
+chown 994:994 -R "${SLURM_DOCKER_PATH}"/munge
+
+
+# Verify cgroupsv2; otherwise, /sys/fs/cgroups gets mounted readonly in
+# containers erroneously.
+# See: https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/managing_monitoring_and_updating_the_kernel/using-cgroups-v2-to-control-distribution-of-cpu-time-for-applications_managing-monitoring-and-updating-the-kernel
+mount | grep -q 'cgroup2 on /sys/fs/cgroup' || echo "WARNING: cgroupsv2 is not being used; make sure 'systemd.unified_cgroup_hierarchy=1' is in the kernel parameters. For Red Hat derivatives, use: 'sudo grubby --update-kernel=ALL --args=\"systemd.unified_cgroup_hierarchy=1\"'"
